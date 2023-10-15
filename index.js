15288a330912fe5cee43b1615b5fb1303fc30012
micahpowch <powchm@catlin.edu>
2023-10-14 23:02:04 -0700

    full site

---
+++ index.js
@@ -0,0 +1,117 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import webp from 'webp-converter'
+import fs from 'fs'
+import sizeOf from 'image-size'
+import sharp from 'sharp'
+import path from 'path'
+const inp = './img'
+const resized = './resized.png'
+const frameOutput = 'outp.webp'
+const animOutput = 'final.png'
+const maxDim = '499'
+webp.grant_permission();
+
+let app = express()
+app.use(cors({origin:'*'}))
+app.use(bodyParser.raw({
+  type:'application/image',
+  limit: '50mb'
+}))
+
+let lastId=0
+
+function startConversion() {
+  lastId++;
+  let id=""+lastId;
+  try{fs.rmdirSync(id,{recursive:true,force:true});}catch(e){}
+  fs.mkdirSync(id);
+  return id;
+}
+function finishConversion(id) {
+  fs.rmdirSync(id,{recursive:true,force:true})
+}
+async function create(image,id) {
+  let size = sizeOf(image);
+
+  // let ratio = Math.min(1,maxDim / Math.max(size.height, size.width));
+  let ratio = maxDim / Math.max(size.height, size.width);
+
+  // resize input image
+  await new Promise(res=>{
+  sharp(image)
+  .resize(Math.round(size.width * ratio), Math.round(size.height * ratio),ratio>10?{kernel: sharp.kernel.nearest}:{})
+  .toFile(id+path.sep + resized, (err, info) => { res() })});
+
+  // remove previous progress dudes
+  fs.rmSync(id+path.sep + frameOutput,{force:true})
+  fs.rmSync(id+path.sep + animOutput,{force:true})
+
+  // convert resized to webp
+  await webp.cwebp(id+path.sep + resized, id+path.sep + frameOutput, "", "-v");
+
+  // convert webp to webp animated
+  let input = [{ "path": id+path.sep + frameOutput, "offset": "+100" },
+               { "path": id+path.sep + frameOutput, "offset": "+200" },
+               { "path": id+path.sep + frameOutput, "offset": "+300" },];
+  input = [input[0], input[0], input[0],]
+  let result = await webp.webpmux_animate(input, id+path.sep + animOutput, "0", "0,0,0,0", '');
+
+  // let finalImage = fs.readFileSync(animOutput)
+  // return finalImage;
+  // console.log(result)
+}
+// await test();
+
+function createWebp(image) {
+
+}
+
+app.post('/image', async (req, res) => {
+  console.log('hi')
+  console.log(req.body)
+  let id = startConversion()
+  try{
+  let output = await create(req.body,id)
+  // res.send(output)
+  console.log(process.cwd() + path.sep + id + path.sep + animOutput)
+  res.sendFile(process.cwd() + path.sep + id + path.sep + animOutput,()=>{finishConversion(id)})
+  // res.end();
+  return;
+  } catch (e) {
+    res.status(400)
+    res.send('could not convert file. make sure to use a common image format.')
+    finishConversion(id)
+  }
+})
+
+app.post('/user', async (req, res) => {
+  let id = startConversion()
+  try{
+  console.log('hi')
+  console.log(req.body)
+  let userRes = await fetch('https://api.scratch.mit.edu/users/' + req.body)
+  let userResJson = await userRes.json()
+  let userResPFPLinkRaw = userResJson['profile']['images']['90x90']
+  let imageLink = userResPFPLinkRaw.replace('90x90','500x500');
+
+  let imageBlob = await (await fetch(imageLink)).blob()
+  console.log(imageBlob)
+  let output = await create(Buffer.from(await imageBlob.arrayBuffer()),id)
+  // res.send(output)
+  console.log(process.cwd() + path.sep + id + animOutput)
+  res.sendFile(process.cwd() + path.sep + id + path.sep + animOutput,()=>{finishConversion(id)})
+  // res.end();
+
+  return;
+  } catch (e) {
+    res.status(400)
+    res.send('could not convert user "' + req.body + '"')
+    finishConversion(id)
+  }
+})
+
+
+
+app.listen(4560)
---
+++ script.js
@@ -0,0 +1,162 @@
+const API = 'http://localhost:4560'
+
+
+
+//////
+let fileElem=document.querySelector("#file")
+let dropArea=document.querySelector('.mainSection')
+let usernameButton=document.querySelector('#usernamebutton')
+let usernameInput=document.querySelector('#usernameinput')
+let converting=document.querySelector('#converting')
+let doneElem=document.querySelector('#done')
+let mylink=document.querySelector('#mylink')
+let errorbox=document.querySelector('#error')
+let errormessage=document.querySelector('#errormessage')
+
+var state=0;
+//ready 0, loading 1, done 2
+
+
+usernameButton.onclick=(e)=>{
+e.preventDefault()
+doStuffWithUsername(usernameInput.value)
+
+}
+usernameInput.addEventListener('keypress',(e)=>{
+    if(e.key=='Enter'){
+        doStuffWithUsername(usernameInput.value)
+    }
+})
+
+
+async function doStuffWithUsername(name) {
+    start()
+    output = await fetch(API+'/user',{
+        method:'post',
+        body:name,
+        headers:{
+            "Content-Type":"application/image",
+        }
+    })
+    if(output.status==400) {errorr(await output.text()); return;}
+
+
+    // console.log(output)
+    // console.log(output.body)
+
+    let blob = await output.blob()
+    console.log(blob)
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    // the filename you want
+    a.download = `${name}-hires.png`;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    done()
+}
+
+async function doShitWithFile(file) {
+    if(state==1) {return}
+    if(state==2) {reset(); return}
+    start()
+    console.log(file)
+
+
+    // let fileReader = new FileReader()
+    // let dataUrl = fileReader.readAsDataURL(file)
+    // fileReader.result
+    // console.log(dataUrl)
+
+    output = await fetch(API+'/image',{
+        method:'post',
+        body:file,
+        headers:{
+            "Content-Type":"application/image",
+        }
+    })
+    if(output.status==400) {errorr(await output.text()); return;}
+
+    // console.log(output)
+    // console.log(output.body)
+
+    let blob = await output.blob()
+    console.log(blob)
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    // the filename you want
+    a.download = `${file.name.split('.')[0]}-hires.png`;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+    // or you know, something with better UX...
+    // alert('your file has downloaded!'); 
+    // console.log(output.body)
+    done();
+}
+
+
+
+function fileChoose(e) {
+    if(state==1) {return}
+    if(state==2) {reset(); return}
+    console.log(e?.target?.getAttribute?.("name"))
+    if(e?.target==usernameInput || e?.target==mylink) {return}
+    let input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = _ => {
+      // you can use this method to get file and perform respective operations
+            doShitWithFile(input.files[0])
+
+          };
+    input.click();
+    
+}
+dropArea.onclick=fileChoose
+
+
+dropArea.ondrop=(e)=>{
+    e.preventDefault();
+    console.log('YOOOOOO!!!')
+    doShitWithFile(e.dataTransfer.files[0])
+}
+dropArea.ondragover=(e)=>{
+    e.preventDefault();
+    console.log(e)
+}
+
+
+
+
+
+function start(){
+    converting.style.display='flex'
+    errorbox.style.display='none'
+    doneElem.style.display='none'
+    state=1;
+    document.documentElement.style.setProperty('--panelRot', '180deg');
+    document.body.style.cursor='unset'
+}
+function done(){
+    state=2;
+    converting.style.display='none'
+    errorbox.style.display='none'
+    doneElem.style.display='flex'
+    document.body.style.cursor='unset'
+}
+function reset(){
+    state=0;
+    document.documentElement.style.setProperty('--panelRot', '0deg');
+    document.body.style.cursor='pointer'
+}
+function errorr(e) {
+    state=2;
+    converting.style.display='none'
+    errorbox.style.display='flex'
+    doneElem.style.display='none'
+    errormessage.innerText = e;
+}
